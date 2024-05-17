7d53511f7777d92fc93354b499ee1019100807c1
lgy <ini03134@naver.com>
2024-05-17 10:59:03 +0900

    #1. todo api 할 일 기능 작성중 (create, select), CORS config

---
+++ src/main/java/com/example/todo/todoapi/dto/request/TodoCreateRequestDTO.java
@@ -0,0 +1,41 @@
+package com.example.todo.todoapi.dto.request;
+
+import com.example.todo.todoapi.entity.Todo;
+import com.example.todo.todoapi.service.TodoService;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+@Setter @Getter @ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@Builder
+public class TodoCreateRequestDTO {
+
+    @NotBlank
+    @Size(min = 2, max = 30)
+    private String title;
+
+    // dto 를 엔터티로 변환
+    public Todo toEntity() {
+        return Todo.builder()
+                .title(title)
+                .build();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
