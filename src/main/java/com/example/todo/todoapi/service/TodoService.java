088909b6d039a21b7e91edd007b234a5515663a6
lgy <ini03134@naver.com>
2024-05-17 12:09:12 +0900

    #2. todo api 할 일 기능 작성 완료

--- src/main/java/com/example/todo/todoapi/service/TodoService.java
+++ src/main/java/com/example/todo/todoapi/service/TodoService.java
@@ -1,6 +1,7 @@
 package com.example.todo.todoapi.service;
 
 import com.example.todo.todoapi.dto.request.TodoCreateRequestDTO;
+import com.example.todo.todoapi.dto.request.TodoModifyRequestDTO;
 import com.example.todo.todoapi.dto.response.TodoDetailResponseDTO;
 import com.example.todo.todoapi.dto.response.TodoListResponseDTO;
 import com.example.todo.todoapi.entity.Todo;
@@ -11,6 +12,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -40,6 +42,33 @@ public class TodoService {
                 .todos(dtoList)
                 .build();
 
+    }
+
+    public TodoListResponseDTO delete(final String todoId) throws Exception{ // 서비스 단에서 매개변수 값 변경 못하도록 final 선언(엄격하게 하려면~)
+
+
+        todoRepository.findById(todoId).orElseThrow(
+                () -> {
+                    log.error("id가 존재하지 않아 삭제에 실패했습니다 - ID: {}", todoId);
+                    throw new RuntimeException("id가 존재하지 않아 삭제에 실패 했습니다.");
+                }
+        );
+        todoRepository.deleteById(todoId);
+
+
+        return findAll();
+    }
+
+    public TodoListResponseDTO update(final TodoModifyRequestDTO requestDTO) throws Exception{
+        Optional<Todo> targetEntity = todoRepository.findById(requestDTO.getId());
+
+        targetEntity.ifPresent(todo -> {   // 해당 아이디에 맞는 엔터티가 존재한다면
+            todo.setDone(requestDTO.isDone()); //  done값을 넣어주고
+
+            todoRepository.save(todo);
+
+        });
+        return findAll();
     }
 }
 
